e809a037277f66a777a59fcde3a4cec2b9dfdeb4
hegomez <hegomez@telecentro.net.ar>
2022-01-13 15:21:17 -0300

    refactor

--- pages/pokecard/[pokemon].js
+++ pages/pokecard/[pokemon].js
@@ -1,7 +1,7 @@
 import React from "react";
 
 export async function getStaticPaths() {
-  const res = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=200&offset=200`);
+  const res = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=248&offset=0`);
   const data = await res.json();
   
   let paths = data.results.map((pokemon) => {
