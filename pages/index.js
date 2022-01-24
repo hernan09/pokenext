c480f407422addc4fe9ae3619f3ecba1452870f6
hegomez <hegomez@telecentro.net.ar>
2022-01-24 18:08:41 -0300

    cache

--- pages/index.js
+++ pages/index.js
@@ -44,18 +44,6 @@ export const getStaticProps = async () => {
   };
 };
 
-const cachedFetch = async (url) => {
-  const cachedResponse = cache.get(url);
-  if (cachedResponse) {
-    return cachedResponse;
-  } else {
-    const hours = 24;
-    const response = await fetch(url);
-    const data = await response.json();
-    cache.put(url, data, hours * 1000 * 60 * 60);
-    return data;
-  }
-};
 
 const Pokemons = ({ arrayPokemon2 }) => {
   const [searchTerm, setSearchTerm] = useState('');
